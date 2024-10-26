e1676a554a02caf7426f8bd8ebfae4b6a5049fcd
Xalegor <Argus1672@gmail.com>
2024-10-26 23:50:40 +0500

    tsconfig.json strict mode -> false

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,3 +1,5 @@
+// src/components/ButtonLink.tsx
+
 import { PrismicNextLink, PrismicNextLinkProps } from "@prismicio/next";
 import clsx from "clsx";
 
@@ -8,7 +10,7 @@ export default function ButtonLink({
   return (
     <PrismicNextLink
       className={clsx(
-        "after:duration-900 relative inline-flex h-fit w-fit rounded border-2 border-blue-100/20 border-blue-800 bg-blue-200/10 bg-blue-900 px-4 py-2 text-white outline-none ring-indigo-600 transition-colors after:absolute after:inset-0 after:z-10 after:animate-pulse after:rounded-xl after:bg-blue-400 after:bg-opacity-0 after:blur-md after:transition-all hover:border-indigo-400/40 hover:text-white after:hover:bg-opacity-20 focus:ring-2",
+        "relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2",
         className,
       )}
       {...restProps}
